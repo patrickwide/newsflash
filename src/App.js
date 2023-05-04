6d74b58c58df951f6158202e7096888588d5da2b
patrickwide <patrickwide254@gmail.com>
2023-05-04 13:35:28 +0300

    merge Search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import NewsList from "./components/NewsList";
 import NEWS_API_KEY from "./data/config";
+import Search from "./components/Search";
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [bookmarkedArticles, setBookmarkedArticles] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -26,6 +28,27 @@ function App() {
       }
     })();
   }, []);
+
+  useEffect(() => {
+    if (searchQuery) {
+      (async () => {
+        setIsLoading(true);
+        try {
+          const response = await fetch(
+            `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${NEWS_API_KEY}`
+          );
+          const data = await response.json();
+          setArticles(data.articles);
+          setIsLoading(false);
+          console.log(data);
+        } catch (error) {
+          console.error("Error fetching news data: ", error);
+          setError(error);
+          setIsLoading(false);
+        }
+      })();
+    }
+  }, [searchQuery]);
 
   // Function to handle bookmarking an article
   const handleBookmark = (article) => {
@@ -47,6 +70,8 @@ function App() {
 
   return (
     <div>
+      {/* Render the Search component and pass the query and setSearchQuery function as props */}
+      <Search onSubmit={(query) => setSearchQuery(query)} />
       {/* Pass the news data as props to the NewsList component */}
       <NewsList articles={articles} bookmarkArticle={handleBookmark} />
     </div>
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -5,8 +5,8 @@ function NewsList({ articles, bookmarkArticle }) {
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        {articles.map((article) => (
-          <div key={article.title}>
+        {articles.map((article, index) => (
+          <div key={index}>
             <NewsItem article={article} bookmarkArticle={bookmarkArticle} />
           </div>
         ))}
