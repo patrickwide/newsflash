51be6f443899681c6e8bef2b90ee95bcdeb132b1
patrickwide <patrickwide254@gmail.com>
2023-05-10 06:13:12 +0300

    Add scrollers to the right and left sidebars in xl view

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -8,15 +8,15 @@ const Layout = ({ header, main, sidebarLeft, sidebarRight }) => {
       <header>{header}</header>
 
       <div className="hidden lg:block lg:container lg:mx-auto">
-        <div class="flex space-x-4">
-          <div class="flex-none w-72">
-            <div className="sticky top-0 py-4">{sidebarLeft}</div>
+        <div className="flex space-x-4">
+          <div className="flex-none w-72">
+            <ScrollableSidebar sidebar={sidebarLeft} />
           </div>
-          <div class="flex-grow">
+          <div className="flex-grow">
             <div className="dark:border-gray-700">{main}</div>
           </div>
-          <div class="flex-none w-72">
-            <div className="sticky top-0 py-4">{sidebarRight}</div>
+          <div className="flex-none w-72">
+            <ScrollableSidebar sidebar={sidebarRight} />
           </div>
         </div>
       </div>
@@ -98,20 +98,20 @@ const Layout = ({ header, main, sidebarLeft, sidebarRight }) => {
         </div>
 
         {activeTab === 1 && (
-          <div class="md:px-10 px-2 pb-4">
-            <div class="max-w-md mx-auto">{sidebarLeft}</div>
+          <div className="md:px-10 px-2 pb-4">
+            <div className="max-w-md mx-auto">{sidebarLeft}</div>
           </div>
         )}
 
         {activeTab === 2 && (
-          <div class="overflow-hidden md:px-10 px-2 pb-4">
-            <div class="w-24 min-w-full mx-auto">{main}</div>
+          <div className="overflow-hidden md:px-10 px-2 pb-4">
+            <div className="w-24 min-w-full mx-auto">{main}</div>
           </div>
         )}
 
         {activeTab === 3 && (
-          <div class="md:px-10 px-2 pb-4">
-            <div class="max-w-md mx-auto">{sidebarRight}</div>
+          <div className="md:px-10 px-2 pb-4">
+            <div className="max-w-md mx-auto">{sidebarRight}</div>
           </div>
         )}
       </div>
@@ -121,160 +121,35 @@ const Layout = ({ header, main, sidebarLeft, sidebarRight }) => {
 
 export default Layout;
 
-// import { useState } from "react";
+const ScrollableSidebar = ({ sidebar }) => {
+  return (
+    <div
+      className="sticky top-0 py-4"
+      style={{ overflowY: "scroll", maxHeight: "calc(100vh)" }}
+    >
+      <style>
+        {`
+          ::-webkit-scrollbar {
+            width: 8px;
+            border-radius: 10px;
+          }
 
-// const Layout = ({ header, main, sidebarLeft, sidebarRight }) => {
-//   const [activeTab, setActiveTab] = useState(1);
+          ::-webkit-scrollbar-track {
+            background: #f1f1f1;
+            border-radius: 10px;
+          }
 
-//   return (
-//     <div>
-//       <div className="md:container md:mx-auto">
-//         {/* <TabComponent /> */}
-//         <div className="lg:block lg:grid lg:grid-cols-4 lg:gap-4 lg:font-mono lg:rounded-lg">
-//           {/*  */}
-//           <div className="lg:p-2 hidden lg:block">
-//             <div className="sticky top-0">{sidebarLeft}</div>
-//           </div>
-//           {/*  */}
-//           <div className="hidden lg:block lg:col-span-2 lg:border-gray-200 lg:dark:border-gray-700">
-//             <div>{main}</div>
-//           </div>
-//           {/*  */}
-//           <div className="lg:p-2 hidden lg:block">
-//             <div className="sticky top-0">{sidebarRight}</div>
-//           </div>
-//         </div>
-//       </div>
+          ::-webkit-scrollbar-thumb {
+            background: #888;
+            border-radius: 10px;
+          }
 
-//       {/* Tabbed view for tablet and mobile */}
-//       <div className="lg:hidden">
-//         <div className="flex justify-center py-4">
-//           <button
-//             className={`mx-1 py-2 px-4 rounded-full ${
-//               activeTab === 1
-//                 ? "bg-blue-500 text-white"
-//                 : "bg-gray-200 text-gray-700"
-//             }`}
-//             onClick={() => setActiveTab(1)}
-//           >
-//             Tab 1
-//           </button>
-
-//           <button
-//             className={`mx-1 py-2 px-4 rounded-full ${
-//               activeTab === 2
-//                 ? "bg-blue-500 text-white"
-//                 : "bg-gray-200 text-gray-700"
-//             }`}
-//             onClick={() => setActiveTab(2)}
-//           >
-//             Tab 2
-//           </button>
-
-//           <button
-//             className={`mx-1 py-2 px-4 rounded-full ${
-//               activeTab === 3
-//                 ? "bg-blue-500 text-white"
-//                 : "bg-gray-200 text-gray-700"
-//             }`}
-//             onClick={() => setActiveTab(3)}
-//           >
-//             Tab 3
-//           </button>
-//         </div>
-
-//         {activeTab === 1 && (
-//           <div className="flex justify-center items-center p-4">
-//             {sidebarLeft}
-//           </div>
-//         )}
-
-//         {activeTab === 2 && <div className="p-4">{main}</div>}
-
-//         {activeTab === 3 && (
-//           <div className="flex justify-center items-center">{sidebarRight}</div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Layout;
-
-// import React, { useState } from "react";
-
-// const Layout = ({ header, main, sidebarLeft, sidebarRight }) => {
-//   const [activeTab, setActiveTab] = useState(2);
-
-//   const handleTabClick = (tabNumber) => {
-//     setActiveTab(tabNumber);
-//   };
-
-//   return (
-//     <div>
-//       <header>{header}</header>
-
-//       <div className="p-4">
-//         <div className="flex rounded-md bg-gray-100">
-//           <button
-//             className={`flex-1 py-2 px-3 rounded-md ${
-//               activeTab === 1
-//                 ? "bg-blue-800 text-white m-1"
-//                 : "m-1 bg-gray-300 hover:bg-blue-200"
-//             } focus:outline-none mr-1`}
-//             onClick={() => handleTabClick(1)}
-//           >
-//             <span className="text-center">Tab 1</span>
-//           </button>
-//           <button
-//             className={`flex-1 py-2 px-3 rounded-md ${
-//               activeTab === 2
-//                 ? "bg-blue-800 text-white my-1"
-//                 : "my-1 bg-gray-300 hover:bg-blue-200"
-//             } focus:outline-none`}
-//             onClick={() => handleTabClick(2)}
-//           >
-//             <span className="text-center">Tab 2</span>
-//           </button>
-//           <button
-//             className={`flex-1 py-2 px-3 rounded-md ${
-//               activeTab === 3
-//                 ? "bg-blue-800 text-white m-1"
-//                 : "m-1 bg-gray-300 hover:bg-blue-200"
-//             } focus:outline-none mr-1`}
-//             onClick={() => handleTabClick(3)}
-//           >
-//             <span className="text-center">Tab 3</span>
-//           </button>
-//         </div>
-//       </div>
-//       <div>
-//         {activeTab === 1 && <div className="sticky top-0">{sidebarLeft}</div>}
-//         {activeTab === 2 && (
-//           <div className="col-span-2 border-gray-200 dark:border-gray-700 px-2">
-//             {main}
-//           </div>
-//         )}
-//         {activeTab === 3 && <div className="sticky top-0">{sidebarRight}</div>}
-//       </div>
-
-//       <div className="md:container md:mx-auto">
-//         {/* <TabComponent /> */}
-//         <div className="grid grid-cols-4 gap-4 font-mono rounded-lg">
-//           <div className="p-2">
-//             <div className="sticky top-0">{sidebarLeft}</div>
-//           </div>
-//           <div className="col-span-2 border-gray-200 dark:border-gray-700">
-//             {main}
-//           </div>
-
-//           <div className="p-2">
-//             <div className="sticky top-0">{sidebarRight}</div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Layout;
+          ::-webkit-scrollbar-thumb:hover {
+            background: #555;
+          }
+        `}
+      </style>
+      <div style={{ padding: "5px" }}>{sidebar}</div>
+    </div>
+  );
+};
